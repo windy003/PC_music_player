7177c4aca79fa9a0e359030783dfc67ce2870db2
windy003 <928920471@qq.com>
2025-07-14 17:47:23 +0800

    更新了版本号,准备编译

--- main.py
+++ main.py
@@ -489,7 +489,7 @@ class MusicPlayer(QMainWindow):
     
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("音乐播放器:2025/07/14-04")
+        self.setWindowTitle("音乐播放器:2025/07/14-05")
         self.setGeometry(100, 100, 800, 600)
         
         # 设置应用图标
