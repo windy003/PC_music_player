7dd837250afa7cf0196be65c43d3e9fb93339abb
windy003 <928920471@qq.com>
2026-08-29 10:54:07 +0800

    调整

--- music_player.cmd
+++ music_player.cmd
@@ -1,9 +1,6 @@
 @echo off
 cd /d "D:\files\using\Python\PC_music_player"
- "D:\files\using\Python\PC_music_player\.venv\Scripts\python.exe"    "D:\files\using\Python\PC_music_player\main.py"
-
-
-cmd  -k
+ start /b  ""  "D:\files\using\Python\PC_music_player\.venv\Scripts\pythonw.exe"    "D:\files\using\Python\PC_music_player\main.py"
 
 
 
